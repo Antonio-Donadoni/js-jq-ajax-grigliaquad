702b225430f94c866b7c3ca72347e76467d0d8b2
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-21 14:59:42 +0200

    ADD JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
+function addNewNumberListener() {
+  var btn = $("#add-number-btn")
+  btn.click(getNewNumber)
+}
+
+function getNewNumber() {
+  var squareTarget = $(".square.active");
+  var nextTarget = $(".square.active").next(".square");
+
+  $.ajax ({
+      url : 'https://flynn.boolean.careers/exercises/api/random/int',
+      method : 'GET',
+      success : function(data, state) {
+
+        var success = data['success'];
+        var value = data['response'];
+
+        if (success) {
+            if (value <= 5) {
+              squareTarget.html(value);
+              squareTarget.addClass("under-five");
+            } else {
+              squareTarget.html(value);
+              squareTarget.addClass("above-five");
+            }
+
+        } else {
+          console.log(error);
+        }
+      },
+      error: function(request, state, error) {
+        console.log('request' , request);
+        console.log('state' , state);
+        console.log('error' , error);
+      }
+    });
 
 
+
+
+  squareTarget.removeClass("active");
+  nextTarget.addClass("active");
+}
 
 function init() {
-console.log("ciao");
+addNewNumberListener();
 }
 
 $( document ).ready(init);
