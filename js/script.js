42a2ca58511428c412c66645c4cf7a9665cae39a
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-21 17:24:02 +0200

    MODIFICA CLICK SU OGNI SQUARE

--- js/script.js
+++ js/script.js
@@ -1,45 +1,42 @@
 function addNewNumberListener() {
-  var btn = $("#add-number-btn")
-  btn.click(getNewNumber)
+  var square = $(".square")
+  square.click(getNewNumber)
 }
 
 function getNewNumber() {
-  var squareTarget = $(".square.active");
-  var nextTarget = $(".square.active").next(".square");
+  var squareTarget = $(this);
 
-  $.ajax ({
-      url : 'https://flynn.boolean.careers/exercises/api/random/int',
-      method : 'GET',
-      success : function(data, state) {
+  if (!(squareTarget.hasClass("under-five") || squareTarget.hasClass("above-five")))
+  {
 
-        var success = data['success'];
-        var value = data['response'];
+    $.ajax ({
+        url : 'https://flynn.boolean.careers/exercises/api/random/int',
+        method : 'GET',
+        success : function(data, state) {
 
-        if (success) {
-            if (value <= 5) {
-              squareTarget.html(value);
-              squareTarget.addClass("under-five");
-            } else {
-              squareTarget.html(value);
-              squareTarget.addClass("above-five");
-            }
+          var success = data['success'];
+          var value = data['response'];
 
-        } else {
-          console.log(error);
+          if (success) {
+              if (value <= 5) {
+                squareTarget.html(value);
+                squareTarget.addClass("under-five");
+              } else {
+                squareTarget.html(value);
+                squareTarget.addClass("above-five");
+              }
+
+          } else {
+            console.log(error);
+          }
+        },
+        error: function(request, state, error) {
+          console.log('request' , request);
+          console.log('state' , state);
+          console.log('error' , error);
         }
-      },
-      error: function(request, state, error) {
-        console.log('request' , request);
-        console.log('state' , state);
-        console.log('error' , error);
-      }
-    });
-
-
-
-
-  squareTarget.removeClass("active");
-  nextTarget.addClass("active");
+      });
+   }
 }
 
 function init() {
